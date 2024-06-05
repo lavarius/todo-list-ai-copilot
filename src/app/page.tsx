05d218d006345308cc4de916f76085ed8e336ec2
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2024-06-05 11:57:47 -0700

    import TodoList and Header for functional Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import Image from "next/image";
+import Header from "./components/Header";
+import { TodoList } from "./components/TodoList";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <>
+        <Header />
+        <div className="border rounded-md max-w-2xl mx-auto p-4 mt-4">
+          <h1 className="text-2xl text-white font-bold mb-4">
+            Create a to-do list
+          </h1>
+          <TodoList />
+        </div>
+      </>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
