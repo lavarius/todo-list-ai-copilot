5d511bf8d8e47763fa13e7bbab4abb4c75fe23c0
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2024-06-05 15:19:38 -0700

    specify CopilotKit backend endpoint and render chatbot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,22 @@ export default function Home() {
           <h1 className="text-2xl text-white font-bold mb-4">
             Create a to-do list
           </h1>
-          <TodoList />
+          <CopilotKit runtimeUrl="/api/copilotkit">
+            <TodoList />
+
+            <CopilotPopup
+              instructions={
+                "Help the user manage a todo list. If the user provides a high level goal, " +
+                "break it down into a few specific tasks and add them to the list"
+              }
+              defaultOpen={true}
+              labels={{
+                title: "Todo List Copilot",
+                initial: "Hi you! 👋 I can help you manage your todo list.",
+              }}
+              clickOutsideToClose={false}
+            />
+          </CopilotKit>
         </div>
       </>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
